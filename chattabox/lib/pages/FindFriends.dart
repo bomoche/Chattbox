eaecd9ab8adc8d79fc43f32f24fe34731a7f8d64
Bongani Moche <bonganimoche8@gmail.com>
2024-01-25 13:47:28 +0200

    find freinds screen

---
+++ chattabox/lib/pages/FindFriends.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FindFrineds extends StatefulWidget {
+  const FindFrineds({super.key});
+
+  @override
+  State<FindFrineds> createState() => _FindFrinedsState();
+}
+
+class _FindFrinedsState extends State<FindFrineds> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
