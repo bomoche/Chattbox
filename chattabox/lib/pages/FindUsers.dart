5c48fbb847bd292d3ca346966ec4ecfa291f2e80
Bongani Moche <bonganimoche8@gmail.com>
2024-01-25 13:46:04 +0200

    find users

---
+++ chattabox/lib/pages/FindUsers.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FindUsers extends StatefulWidget {
+  const FindUsers({super.key});
+
+  @override
+  State<FindUsers> createState() => _FindUsersState();
+}
+
+class _FindUsersState extends State<FindUsers> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
