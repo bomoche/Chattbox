ad736007fc2c2699d466c53040236c0ec1bc3de8
Bongani Moche <bonganimoche8@gmail.com>
2024-01-25 09:47:40 +0200

    updated main

--- chattabox/lib/main.dart
+++ chattabox/lib/main.dart
@@ -3,6 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
-  runApp(const LoginPage());
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Chattabox',
+      home: LoginPage(),
+    );
+  }
 }
